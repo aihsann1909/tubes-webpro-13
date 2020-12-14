798e8cb65a0470a4caf6edb92cd3758e74611e0c
masjapa <maasjapaa@gmail.com>
2020-12-14 22:47:28 +0700

    first commit

---
+++ application/views/about.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>about us</title>
+</head>
+
+<body>
+
+	<div id="container">
+		<h1>This is about us</h1>
+	</div>
+
+	<!-- Tambahanya Dari Afif -->
+
+
+</body>
+
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>home</title>
+</head>
+<body>
+
+<div id="container">
+    <h1>This is home</h1>
+    <a href="<?= base_url('welcome')?>">ke halaman utama</a>
+    <a href="<?= base_url('about')?>">ke halaman about</a>
+</div>
+
+</body>
+</html>
